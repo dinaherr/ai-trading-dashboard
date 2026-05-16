06770da2d6c5663525ce6d535e0eeb1fa2a66858
dinaherr <dinaherr.digital@gmail.com>
2026-05-15 22:12:43 -0500

    Update app.py
    added alpha api key call

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-api_key = st.secrets["OPENAI_API_KEY"]
+openai_key = st.secrets["OPENAI_API_KEY"]
+alpha_key = st.secrets["ALPHA_VANTAGE_API_KEY"]
 
+st.write("Keys loaded successfully!")
 st.write("API key loaded successfully!")
